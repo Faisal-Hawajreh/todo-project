a795479f33c7bbb04617c2718f2109992fd4148e
Faisal Al-Hawajreh <faisal.alhawajreh@gmail.com>
2022-02-02 01:50:58 +0200

    fix everything

--- JS/app.js
+++ JS/app.js
@@ -6,10 +6,10 @@ if(gender == "male" || gender == "Male"){
     var MrOrMs = "Mr";
 }
 else if(gender == "female" || gender == "Female"){
-    var MrOrMs = "Ms";
+    MrOrMs = "Ms";
 }
 else{
-    var MrOrMs = ""
+    MrOrMs = ""
 };
 //try var instead of let and see what happen
 var age = prompt("Enter your age")
@@ -22,47 +22,54 @@ else{
 if(Continue == true){
     alert("Welcome to our World \n"+ MrOrMs +" "+ Name +" ("+age+ ")")
 }
-
-
-var LikePepsi = prompt("Do you like Pepsi?","Yes or No");
-if(LikePepsi == "Yes" || LikePepsi == "yes"){
-        var LikePepsi = "yes";
-    }
-    else if(LikePepsi == "No" || LikePepsi == "no"){
-        var LikePepsi = "no";
-    }
-    else{
-        var LikePepsi = "Invalid"
-    };
-
-var LikeBurger = prompt("Do you like Burger?","Yes or No");
-if(LikeBurger == "Yes" || LikeBurger == "yes"){
-    var LikeBurger = "yes";
-}
-else if(LikeBurger == "No" || LikeBurger == "no"){
-    var LikeBurger = "no";
-}
-else{
-    var LikeBurger = "Invalid";
-};
-
-var LikeFries = prompt("Do you like Fries?","Yes or No");
-if(LikeFries == "Yes" || LikeFries == "yes"){
-    var LikeFries = "yes";
-}
-else if(LikeFries == "No" || LikeFries == "no"){
-    var LikeFries = "no";
-}
-else{
-    var LikeFries = "Invalid";
-};
-
-
 var LikesArr = [];
-LikesArr.unshift(LikePepsi,LikeBurger,LikeFries);
+for(let i = 0;i<3;i++){
+        if(i==0){
+        var LikePepsi = prompt("Do you like Pepsi?","Yes or No");
+        if(LikePepsi == "Yes" || LikePepsi == "yes"){
+                LikePepsi = "yes";
+            }
+            else if(LikePepsi == "No" || LikePepsi == "no"){
+                LikePepsi = "no";
+            }
+            else{
+                LikePepsi = "Invalid"
+            }
+            LikesArr.push(LikePepsi)
+    }else if(i==1){
+        var LikeBurger = prompt("Do you like Burger?","Yes or No");
+        if(LikeBurger == "Yes" || LikeBurger == "yes"){
+            LikeBurger = "yes";
+        }
+        else if(LikeBurger == "No" || LikeBurger == "no"){
+            LikeBurger = "no";
+        }
+        else{
+            LikeBurger = "Invalid";
+        }
+        LikesArr.push(LikeBurger)
+    }else{
+        var LikeFries = prompt("Do you like Fries?","Yes or No");
+        if(LikeFries == "Yes" || LikeFries == "yes"){
+            LikeFries = "yes";
+        }
+        else if(LikeFries == "No" || LikeFries == "no"){
+            LikeFries = "no";
+        }
+        else{
+            LikeFries = "Invalid";
+        }
+        LikesArr.push(LikeFries)
+    }
+}
+
+console.log(LikesArr)
+// var LikesArr = [];
+
+// LikesArr.push(LikePepsi,LikeBurger,LikeFries);
 
 function ShowLikesArr(){
     console.log(LikesArr.toString())
     alert(LikesArr)
 }
-ShowLikesArr();
+ShowLikesArr()
